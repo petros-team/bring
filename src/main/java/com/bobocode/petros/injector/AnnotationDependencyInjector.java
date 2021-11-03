31d591fdd9cb1546a18cdaf85a6c3543735dccc3
stas <stas.zabramnyi@gmail.com>
2021-11-03 20:15:55 +0200

    bring-16 implemented injector

--- src/main/java/com/bobocode/petros/injector/AnnotationDependencyInjector.java
+++ src/main/java/com/bobocode/petros/injector/AnnotationDependencyInjector.java
@@ -5,8 +5,8 @@ import com.bobocode.petros.scaner.AnnotationDependencyClassScanner;
 import com.bobocode.petros.scaner.AnnotationDependencyConfigurationScanner;
 import com.bobocode.petros.scaner.DependencyScanner;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.util.*;
 
 @SuppressWarnings("all")
 public class AnnotationDependencyInjector implements DependencyInjector {
@@ -24,10 +24,41 @@ public class AnnotationDependencyInjector implements DependencyInjector {
 
     @Override
     public Map<DependencyDefinition, Object> injectedDependencyDefinitionObjectMap() {
-//        var map1 = classScanner.scan(packageName);
-//        var map2 = configurationScanner.scan(packageName);
+        Map<String, List<DependencyDefinition>> map1 = classScanner.scan(packageName);
+
+//        Map<String, List<DependencyDefinition>> map2 = configurationScanner.scan(packageName);
         return null;
     }
 
+    private Object createInstance(DependencyDefinition definition) {
+        Object instance = definition.getDependencyClass();
+        Set<DependencyDefinition> injectedDependencyDefinitions = definition.getDependencyDefinitions();
 
+        if (!injectedDependencyDefinitions.isEmpty()) {
+            for (DependencyDefinition dependency : injectedDependencyDefinitions) {
+                findField(instance, dependency.getDependencyClass().getClass())
+                        .ifPresent(field -> {
+                            Object instanceToBeInjected = dependencyMap
+                                    .computeIfAbsent(dependency, d -> createInstance(dependency));
+                            setPriveteField(instance, field, instanceToBeInjected);
+                        });
+            }
+        }
+        return instance;
+    }
+
+    private <T> void setPriveteField(T obj, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+            field.setAccessible(false);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private <T> Optional<Field> findField(T obj, Class<?> type) {
+        return Arrays.stream(obj.getClass().getDeclaredFields())
+                .filter(f -> f.getType().isInstance(type)).findFirst();
+    }
 }
