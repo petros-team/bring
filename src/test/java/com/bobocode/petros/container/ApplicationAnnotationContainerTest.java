e1db6479da33a134cc213cc2b9ba4a70e7e5fa36
Serhii Feshchuk <jeoparrdy@gmail.com>
2021-11-03 08:56:22 +0200

    BRING-12: ApplicationAnnotationContainerTest changed

--- src/test/java/com/bobocode/petros/container/ApplicationAnnotationContainerTest.java
+++ src/test/java/com/bobocode/petros/container/ApplicationAnnotationContainerTest.java
@@ -4,21 +4,35 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 class ApplicationAnnotationContainerTest {
     private ApplicationAnnotationContainer sut;
+    private Map<String, Object> dependencies = new HashMap<>();
 
     @BeforeEach
     void setup(){
         sut = new ApplicationAnnotationContainer("some.package");
+        Class<? extends ApplicationAnnotationContainer> testContainer = sut.getClass();
+        dependencies.put("name",Object.class);
+        try {
+            Field field = testContainer.getDeclaredField("dependencyMap");
+            field.setAccessible(true);
+            field.set(sut, dependencies);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     void getDependencyByNameAndClass() {
-        Assertions.assertNull(sut.getDependency("name", Object.class));
+        Assertions.assertNotNull(sut.getDependency("name", Object.class));
     }
 
     @Test
     void getDependencyByClass() {
-        Assertions.assertNull(sut.getDependency(Object.class));
+        Assertions.assertNotNull(sut.getDependency(Object.class));
     }
 }
