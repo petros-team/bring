5cd360a3233fde598f343b51bc61b9ea8152eb09
dp <podeole@gmail.com>
2021-11-10 22:06:13 +0200

    BRING-26: config class dependencies injector flow refactored

--- src/test/java/com/bobocode/petros/injector/AnnotationDependencyInjectorTest.java
+++ src/test/java/com/bobocode/petros/injector/AnnotationDependencyInjectorTest.java
@@ -1,20 +1,41 @@
 package com.bobocode.petros.injector;
 
-import org.junit.jupiter.api.Assertions;
+import com.bobocode.petros.container.DependencyDefinition;
+import com.bobocode.petros.scaner.AnnotationDependencyConfigurationScanner;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class AnnotationDependencyInjectorTest {
     private AnnotationDependencyInjector sut;
+    private AnnotationDependencyConfigurationScanner configurationScanner;
+    private static final String TEST_TARGET_PATH = "target/test-classes/";
+    private static final String TEST_PACKAGE = "com.bobocode.petros.testpackage";
 
+    @SneakyThrows
     @BeforeEach
     void setup(){
-        sut = new AnnotationDependencyInjector("some.package");
+        configurationScanner = new AnnotationDependencyConfigurationScanner();
+        var configScannerClass = configurationScanner.getClass();
+        var targetClassesPathField = configScannerClass.getDeclaredField("targetClassesPath");
+        targetClassesPathField.setAccessible(true);
+        targetClassesPathField.set(configurationScanner, TEST_TARGET_PATH);
+        sut = new AnnotationDependencyInjector(TEST_PACKAGE);
+        final Class<? extends AnnotationDependencyInjector> sutClass = sut.getClass();
+        final Field configScannerField = sutClass.getDeclaredField("configurationScanner");
+        configScannerField.setAccessible(true);
+        configScannerField.set(sut, configurationScanner);
     }
 
     @Test
     void injectedDependencyDefinitionObjectMap() {
-        Assertions.assertNull(sut.injectedDependencyDefinitionObjectMap());
+        final Map<DependencyDefinition, Object> dependencyDefinitionObjectMap = sut.injectedDependencyDefinitionObjectMap();
+        assertEquals(dependencyDefinitionObjectMap.size(), 5);
     }
 }
